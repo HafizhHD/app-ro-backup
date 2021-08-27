944a69ea6f400c2cdb1d5707a730ca5172c39777
Devin <devin@MacBook-Pro.M1>
2021-08-28 02:30:08 +0700

    Update API for Location.

--- lib/model/rk_child_location_model.dart
+++ lib/model/rk_child_location_model.dart
@@ -1,22 +1,36 @@
-class LocationChild {
-  final String? id;
-  final String? email;
-  final dynamic location;
-  final String? dateHistory;
+import 'package:ruangkeluarga/global/global_formatter.dart';
 
-  LocationChild({
-    this.id,
-    this.email,
-    this.location,
-    this.dateHistory
-    });
+class LocationChild {
+  // final String id;
+  final String email;
+  final Location location;
+  final String dateHistory;
+  final String dateCreate;
+  final DateTime createdAt;
+
+  LocationChild({required this.email, required this.location, required this.dateHistory, required this.dateCreate, required this.createdAt});
 
   factory LocationChild.fromJson(Map<String, dynamic> json) {
     return LocationChild(
-      id: json['_id'] as String?,
-      email: json['emailUser'] as String?,
-      location: json['location'] as dynamic,
-      dateHistory: json['dateTimeHistory'] as String?,
+      // id: json['_id'],
+      email: json['emailUser'],
+      location: Location.fromJson(json['location']),
+      dateHistory: json['dateTimeHistory'],
+      dateCreate: strToDate_EddMMMyyyyHHmm(json['dateCreate']),
+      createdAt: DateTime.parse(json['dateCreate']),
     );
   }
+}
+
+class Location {
+  final String place;
+  final String type;
+  final List coordinates;
+
+  Location({required this.place, required this.type, required this.coordinates});
+
+  Location.fromJson(Map<String, dynamic> json)
+      : place = json['place'],
+        type = json['type'],
+        coordinates = json['coordinates'];
 }
