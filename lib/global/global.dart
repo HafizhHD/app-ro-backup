694459e42293edf40c434ad3f57ea6cebf148172
Devin <devin@MacBook-Pro.M1>
2021-09-16 16:10:09 +0700

    Update Child Permission for camera and audio

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -7,6 +7,8 @@ export 'global_formatter.dart';
 export 'global_snackbar.dart';
 export 'global_enum.dart';
 
+import 'package:permission_handler/permission_handler.dart';
+
 ///CONSTANT
 const keyboardHeight = 150;
 const appName = "Keluarga HKBP";
@@ -33,3 +35,19 @@ const ApkDownloadURL_HKBP = 'https://drive.google.com/drive/folders/1nlj_EiHgD6g
 
 final urlPP = 'https://keluargahkbp.com/toc/privacy_policy_bahasa.html';
 final urlTOC = 'https://keluargahkbp.com/toc/toc_bahasa.html';
+
+Future<bool> childNeedPermission() async {
+  final locationHandler = await Permission.location.status;
+  final contactHandler = await Permission.contacts.status;
+  final phoneHandler = await Permission.phone.status;
+  final cameraHandler = await Permission.camera.status;
+  final audioHandler = await Permission.microphone.status;
+  // final smsHandler = await Permission.sms.status;
+  print('Permision Status location : $locationHandler');
+  print('Permision Status contact : $contactHandler');
+  print('Permision Status phone : $phoneHandler');
+  print('Permision Status camera : $cameraHandler');
+  print('Permision Status microphone : $audioHandler');
+  // print('Permision Status sms : $smsHandler');
+  return locationHandler.isDenied || contactHandler.isDenied || phoneHandler.isDenied || cameraHandler.isDenied || audioHandler.isDenied;
+}
