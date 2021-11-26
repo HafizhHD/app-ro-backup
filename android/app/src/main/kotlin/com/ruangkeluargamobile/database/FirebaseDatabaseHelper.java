f3900920aa1f668d4bb9269ac3f86eda771ba03d
Yosua Willy Handika <willy.handika@dwidasa.com>
2021-11-26 15:39:09 +0700

    update background service

---
+++ android/app/src/main/kotlin/com/ruangkeluargamobile/database/FirebaseDatabaseHelper.java
@@ -0,0 +1,46 @@
+package com.ruangkeluargamobile.database;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ruangkeluargamobile.listener.DataStatus;
+import com.ruangkeluargamobile.model.DataAplikasi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirebaseDatabaseHelper {
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
+    private List<DataAplikasi> aplikasiList = new ArrayList<>();
+
+    public FirebaseDatabaseHelper(){
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference("dataAplikasiFita");
+    }
+
+    public void readDataFirebase(final DataStatus dataStatus){
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                aplikasiList.clear();
+                List<String> keys = new ArrayList<>();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    keys.add(dataSnapshot.getKey());
+                    DataAplikasi dataAplikasi = dataSnapshot.getValue(DataAplikasi.class);
+                    aplikasiList.add(dataAplikasi);
+                }
+                dataStatus.DataLoaded(aplikasiList, keys);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
