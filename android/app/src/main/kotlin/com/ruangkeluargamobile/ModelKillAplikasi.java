3f7841af9184d81421426acaed69a282379422de
Yosua Willy Handika <willy.handika@dwidasa.com>
2021-11-30 19:12:38 +0700

    label informasi

--- android/app/src/main/kotlin/com/ruangkeluargamobile/ModelKillAplikasi.java
+++ android/app/src/main/kotlin/com/ruangkeluargamobile/ModelKillAplikasi.java
@@ -4,14 +4,16 @@ public class ModelKillAplikasi {
     int Uuid;
     int pid;
     String packageId;
+    String appName;
 
     public ModelKillAplikasi() {
     }
 
-    public ModelKillAplikasi(int uuid, int pid, String packageId) {
+    public ModelKillAplikasi(int uuid, int pid, String packageId, String appName) {
         Uuid = uuid;
         this.pid = pid;
         this.packageId = packageId;
+        this.appName = appName;
     }
 
     public int getUuid() {
@@ -36,5 +38,13 @@ public class ModelKillAplikasi {
 
     public void setPid(int pid) {
         this.pid = pid;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
     }
 }
