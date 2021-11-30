297e117e547aab252b132e19d92563c7dbc32ed5
Yosua Willy Handika <willy.handika@dwidasa.com>
2021-11-30 19:38:30 +0700

    label informasi

--- android/app/src/main/kotlin/com/ruangkeluargamobile/LockScreen.java
+++ android/app/src/main/kotlin/com/ruangkeluargamobile/LockScreen.java
@@ -1,7 +1,10 @@
 package com.ruangkeluargamobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,6 +13,7 @@ import com.keluargahkbp.R;
 public class LockScreen extends Activity {
     String appName = "";
     TextView textDesc;
+    Button buttonCLose;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +26,26 @@ public class LockScreen extends Activity {
         }
         textDesc.setText("Keluarga HKBP melakukan blokir aplikasi "+appName+" karena saat ini aplikasi tersebut dibatasi oleh Orangtua.");
         ServiceBackground.getInstance().killProses = true;
+        buttonCLose = (Button) findViewById(R.id.buttonCLose);
+        buttonCLose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LockScreen.this, MainAplication.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         ServiceBackground.getInstance().killProses = false;
+        Intent intent = new Intent(LockScreen.this, MainAplication.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
